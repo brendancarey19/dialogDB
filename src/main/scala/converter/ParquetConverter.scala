d58bcbfd890aa02fcf7b3ca2fd1d3c51f27aa9e5
brendancarey19 <64050030+brendancarey19@users.noreply.github.com>
2023-09-28 16:54:08 -0400

    ex files

--- src/main/scala/converter/ParquetConverter.scala
+++ src/main/scala/converter/ParquetConverter.scala
@@ -10,7 +10,7 @@ object ParquetConverter {
     val spark = SparkManager.spark
 
     def toParquet(csvPath: Path, parquetPath: Path): IO[Unit] = IO {
-        val df = spark.read.option("header", "true").csv(csvPath.toString)
+        val df = spark.read.option("header", "true").option("delimiter", ";").csv(csvPath.toString)
         df.write.mode(SaveMode.Overwrite).parquet(parquetPath.toString)
     }
 }
